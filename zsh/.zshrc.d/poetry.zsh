28781648ccbcf4d6ace8ff3e73f8cc349255ac0d
v-crn <var.cyrano@gmail.com>
2021-01-25 19:47:18 +0900

    fix comment in poetry.zsh

--- zsh/.zshrc.d/poetry.zsh
+++ zsh/.zshrc.d/poetry.zsh
@@ -1,9 +1,8 @@
-"""
-[darvid/zsh-poetry: 🐚 Simple ZSH plugin for automatically activating and deactivating Poetry-created virtualenvs. 🐍](https://github.com/darvid/zsh-poetry)
+# [darvid/zsh-poetry: 🐚 Simple ZSH plugin for automatically activating and deactivating Poetry-created virtualenvs. 🐍](https://github.com/darvid/zsh-poetry)
+#
+# Automatically activates virtual environments created by Poetry when changing to a project directory with a valid pyproject.toml.
+#
+# Also patches poetry shell to work more reliably, especially in environments using pyenv.
 
-Automatically activates virtual environments created by Poetry when changing to a project directory with a valid pyproject.toml.
-
-Also patches poetry shell to work more reliably, especially in environments using pyenv.
-"""
 zinit ice has"poetry"
 zinit load darvid/zsh-poetry
