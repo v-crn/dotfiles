ee90581b5038b46bfdfcc2e9481593ac809dda35
v-crn <var.cyrano@gmail.com>
2021-01-27 08:29:30 +0900

    add aliases for gcloud

---
+++ zsh/.zshrc.d/gcloud.alias.zsh
@@ -0,0 +1,22 @@
+alias gcl='gcloud'
+
+alias gcla='gcloud auth'
+alias gclal='gcloud auth login'
+
+alias gclp='gcloud projects'
+alias gclpl='gcloud projects list'
+
+alias gclcon='gcloud config'
+alias gclconl='gcloud config list'
+
+alias gclcons='gcloud config set'
+alias gclcons-p='gcloud config set compute/project'
+alias gclcons-a='gcloud config set compute/account'
+alias gclcons-r='gcloud config set compute/region'
+alias gclcons-z='gcloud config set compute/zone'
+
+alias gclconc='gcloud config configurations'
+alias gclconc-l='gcloud config configurations list'
+alias gclconc-c='gcloud config configurations create'
+alias gclconc-a='gcloud config configurations activate'
+alias gclconc-ad='gcloud config configurations activate default'
