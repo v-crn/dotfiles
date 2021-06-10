6b8e1b244a1d44ef70d09847bcb1b6fe295f116f
v-crn <var.cyrano@gmail.com>
2021-06-10 18:40:54 +0900

    add docker-compose aliases

--- .zsh/.zshrc.d/docker-compose.zsh
+++ .zsh/.zshrc.d/docker-compose.zsh
@@ -1,2 +1,6 @@
 zinit ice from"gh-r" as"program" mv"docker* -> docker-compose" bpick"*linux*"
 zinit load docker/compose
+
+alias dc='docker-compose'
+alias dcbuild='docker-compose build'
+alias dcup='docker-compose up'
