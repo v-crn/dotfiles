8b36063a30122f0aa6088ce35702eaad33e5ed4d
v-crn <var.cyrano@gmail.com>
2021-08-24 00:51:46 +0900

    update google-cloud-sdk

--- .zsh/.zshenv.d/google-cloud-sdk.path.zsh
+++ .zsh/.zshenv.d/google-cloud-sdk.path.zsh
@@ -1,9 +1,13 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '~/google-cloud-sdk/path.zsh.inc' ]; then . '~/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then
+    . "$HOME/google-cloud-sdk/path.zsh.inc"
 
-# The next line enables shell command completion for gcloud.
-if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then . '~/google-cloud-sdk/completion.zsh.inc'; fi
+    if [ ! $(echo $PATH | grep 'google-cloud-sdk') ]; then
+        export CLOUDSDK_ROOT_DIR="$HOME/google-cloud-sdk"
+        export PATH="$CLOUDSDK_ROOT_DIR/bin:$PATH"
+    fi
+fi
 
 _command_exists python || return
 
-export CLOUDSDK_PYTHON=~/.pyenv/versions/3.7.11/envs/google-cloud-sdk-python/bin/python
+export CLOUDSDK_PYTHON="$HOME/.pyenv/versions/3.7.11/envs/google-cloud-sdk-python/bin/python"
--- .zsh/.zshrc.d/google-cloud-sdk.comp.zsh
+++
@@ -1,2 +0,0 @@
-# The next line enables shell command completion for gcloud.
-if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then . '~/google-cloud-sdk/completion.zsh.inc'; fi
