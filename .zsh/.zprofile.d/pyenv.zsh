a2db27c643fa446c4f727a04b1035820b032685c
v-crn <var.cyrano@gmail.com>
2022-02-23 09:34:07 +0900

    remove pyenv.zsh in .zprofile

--- .zsh/.zprofile.d/pyenv.zsh
+++
@@ -1,9 +0,0 @@
-if [ -e "/usr/local/bin/pyenv" ]; then
-    # For pyenv installed from HomeBrew on macOS
-    eval "$(pyenv init --path)"
-elif [ -e "$HOME/.pyenv" ]; then
-    # For pyenv installed from GitHub
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init --path)"
-fi
