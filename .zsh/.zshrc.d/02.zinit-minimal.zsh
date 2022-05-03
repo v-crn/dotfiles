8f1cc65cd6f668401968b0509f7b941fb2b09ee8
koseihirano <var.cyrano@gmail.com>
2022-05-04 00:59:34 +0900

    substitute zdharma-continuum for zdharma

--- .zsh/.zshrc.d/02.zinit-minimal.zsh
+++ .zsh/.zshrc.d/02.zinit-minimal.zsh
@@ -1,10 +1,10 @@
 zinit wait lucid light-mode for \
     atinit"zicompinit; zicdreplay" \
-    zdharma/fast-syntax-highlighting \
+    zdharma-continuum/fast-syntax-highlighting \
     atload"_zsh_autosuggest_start" \
     zsh-users/zsh-autosuggestions \
     blockf atpull'zinit creinstall -q .' \
     zsh-users/zsh-completions
 
 # history-search-multi-word with Ctrl+R
-zinit light zdharma/history-search-multi-word
+zinit light zdharma-continuum/history-search-multi-word
