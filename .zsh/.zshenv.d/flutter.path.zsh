d19082e2741e0504ba8c186d155e9c6306ffcb81
v-crn <var.cyrano@gmail.com>
2022-10-13 06:44:06 +0900

    update flutter path

--- .zsh/.zshenv.d/flutter.path.zsh
+++ .zsh/.zshenv.d/flutter.path.zsh
@@ -2,4 +2,6 @@ if [ -e "$HOME/fvm/default/bin" ]; then
     export PATH="$PATH":"$HOME/fvm/default/bin"
 elif [ -e "$HOME/flutter/bin" ]; then
     export PATH="$PATH":"$HOME/flutter/bin"
+elif [ -e "$HOME/snap/flutter/common/flutter/bin/flutter" ]; then
+    export PATH="$PATH":"$HOME/snap/flutter/common/flutter/bin"
 fi
