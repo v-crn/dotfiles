a013f3e45eca600eb7a232ebcfb56c7e086ecbb1
v-crn <var.cyrano@gmail.com>
2022-10-16 07:22:27 +0900

    update nvcc and nvidia-smi

--- .zsh/.zshenv.d/nvcc.path.zsh
+++ .zsh/.zshenv.d/nvcc.path.zsh
@@ -1,3 +1,12 @@
+### cuDNN
+
+######################################################################
+# - [NVCC :: CUDA Toolkit Documentation](https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html)
+# - DNN で使われる基本的な機能をまとめた CUDA ライブラリ
+# - フレームワークごとに CUDA のコードを書くムダを無くしている
+# - NVIDIA 自身が最適化しているので性能的にも良いものを手軽に作れる
+######################################################################
+
 if [ -e "/usr/local/cuda/bin" ]; then
     export PATH="/usr/local/cuda/bin:$PATH"
 fi
--- .zsh/.zshenv.d/nvidia-smi.path.zsh
+++ .zsh/.zshenv.d/nvidia-smi.path.zsh
@@ -1,7 +1,10 @@
+### CUDA (Toolkit)
+
+######################################################################
+# - 本来は画像処理用の演算装置である GPU で汎用的な数値計算ができるように NVIDIA が開発したプログラミング環境
+# - コンパイラやライブラリ、API などが含まれる
+######################################################################
+
 if [ -e "/usr/lib/wsl/lib" ]; then
     export PATH="/usr/lib/wsl/lib:$PATH"
-fi
-
-if [ -e "/usr/local/cuda/lib64" ]; then
-    export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
 fi
