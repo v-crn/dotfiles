6fc020dfd9be87b2f6f421625e5ba0fee643cfbf
v-crn <var.cyrano@gmail.com>
2025-02-06 00:09:28 +0900

    update dotnet path

--- .zsh/.zshenv.d/dotnet.path.zsh
+++ .zsh/.zshenv.d/dotnet.path.zsh
@@ -1,4 +1,8 @@
 # .Net SDK path
+if [ -e "$HOME/.dotnet" ]; then
+    export DOTNET_ROOT=$HOME/.dotnet
+    export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+fi
 if [ -e "/usr/local/share/dotnet" ]; then
     export PATH="${PATH}:/usr/local/share/dotnet"
 fi
